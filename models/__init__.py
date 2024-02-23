1e726b4044b53e80e34c65a2b91d8e595a25754d
Relay90 <motaungr511@gmail.com>
2024-02-22 22:20:15 -0800

    AirBnB_clone

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""Initialization of the FileStorage instance"""
+"""
+Module: __init__.py
+"""
+from models.engine import file_storage
 
-from models.engine.file_storage import FileStorage
-
-def initialize_storage():
-    """Initialize FileStorage instance and reload data."""
-    storage = FileStorage()
-    storage.reload()
-
-if __name__ == "__main__":
-    initialize_storage()
+storage = file_storage.FileStorage()
+storage.reload()
